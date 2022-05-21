0d8b08d03d7707fabd85f94d862937e76619dde9
Julie Crotzer <jcrotzerrn@aol.com>
2022-05-21 12:40:20 -0500

    updated index.html and css

--- flask_app/controllers/students.py
+++ flask_app/controllers/students.py
@@ -1,8 +1,7 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.student import Student
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def index():
